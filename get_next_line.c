2b39ce311c9b9804ec40b8ef19209440fb97b3ca
jwasp <nigushaa@yandex.ru>
2020-12-25 16:49:18 +0300

    convert spaces to tab

--- get_next_line.c
+++ get_next_line.c
@@ -47,23 +47,23 @@ int		free_str(char *str)
 }
 int get_next_line_cleanup(char* ptr1, char* ptr2)
 {
-  free(ptr1);
-  free(ptr2);
-  return (-1);
+	free(ptr1);
+	free(ptr2);
+	return (-1);
 }
 
 // ft_strchr("abcdef", 'd') -> "def"
 char*	ft_strchr(char *str, int ch)
 {
-    if (!str)
-        return (NULL);
-    while (*str != '\0')
-    {
-        if (*str == ch)
-            return (str);
+	if (!str)
+		return (NULL);
+	while (*str != '\0')
+	{
+		if (*str == ch)
+			return (str);
 		str++;
-    }
-    return (NULL);
+	}
+	return (NULL);
 }
 
 int		get_next_line(int fd, char **line)
@@ -74,41 +74,42 @@ int		get_next_line(int fd, char **line)
 	char			*tmp;
 
 	if (!line) 
-        return (-1);
-  
+		return (-1);
+
+	// 
+
 	if (ft_strchr(remain, '\n'))
 	{
 		if (!(*line = save_before_n(remain)))
-           return (get_next_line_cleanup(remain, *line));
-        tmp = remain;
+			return (get_next_line_cleanup(remain, *line));
+		tmp = remain;
 		if (!(remain = ft_strdup(pointer_after_n(remain))))
-           return (get_next_line_cleanup(tmp, *line));
-      	free(tmp);
+			return (get_next_line_cleanup(tmp, *line));
+		free(tmp);
 		return (1);
 	}
 	while ((reader = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[reader] = '\0';
+		// remain = remain |+| buffer
+		// 
 		if (ft_strchr(buffer, '\n'))
 		{
-			// buffer: "some line\nmore data"
-			//                   ^
-			//                   \0
 			*(ft_strchr(buffer, '\n')) = '\0';
-            if (!(*line = ft_strjoin(remain ? remain : "", buffer)))
-                return(get_next_line_cleanup(remain, NULL));
-            
-          	tmp = remain;
+			if (!(*line = ft_strjoin(remain ? remain : "", buffer)))
+				return(get_next_line_cleanup(remain, NULL));
+			
+			tmp = remain;
 			if (!(remain = ft_strdup(buffer + ft_strlen(buffer) + 1)))
-           	   	return (get_next_line_cleanup(tmp, *line));
-            free(tmp);
-            
-            return (1);
+				return (get_next_line_cleanup(tmp, *line));
+			free(tmp);
+			
+			return (1);
 		}
-      	tmp = remain;
+		tmp = remain;
 		if (!(remain = ft_strjoin(remain ? remain : "", buffer)))
-          	return (get_next_line_cleanup(tmp, *line));
-      	free(tmp);
+			return (get_next_line_cleanup(tmp, *line));
+		free(tmp);
 	}
 	// printf("reader = %d remain = |%s|\n", reader, remain ? remain : "");
 	if (reader == 0)
@@ -121,46 +122,12 @@ int		get_next_line(int fd, char **line)
 	return (-1);
 }
 
-// char	*line = 1;
-//
-// 		empty.txt - ""
-// get_next_line(fd, &line) -> line = <undefined>, return 0
-// get_next_line(fd, &line) -> line = <undefined>, return 0
-//
-//		one_new_line.txt: "\n"
-// get_next_line(fd, &line) -> line = "", return 1
-// get_next_line(fd, &line) -> line = <undefined>, return 0
-//
-//		cheeky_two_lines.txt: "\nsomeline"
-// get_next_line(fd, &line) -> line = "", return 1
-// get_next_line(fd, &line) -> line = "someline", return 1
-// get_next_line(fd, &line) -> line = <undefined>, return 0
-//
-//		one_word.txt: "Hello\n"
-//                     ^^^^^^
-//                     read(fd, buffer, 7)
-// get_next_line(fd, &line) -> line = "Hello", return 1
-// get_next_line(fd, &line) -> line = "", return 0
-// get_next_line(fd, &line) -> line = "", return 0
-//
-// 		one_line.txt - "just one line\n"
-// get_next_line(fd, &line) -> line = "just one line", return 0
-// get_next_line(fd, &line) -> line = <undefined>, return 0
-//
-// 		file: "First line.\nSecond line.\nThird line.\n"
-// get_next_line(fd, &line) -> line = "First line.", i = 1
-// get_next_line(fd, &line) -> line = "Second line.", i = 1
-// get_next_line(fd, &line) -> line = "Third line.", i = 1
-// get_next_line(fd, &line) -> line = <undefined>, i = 0
-//
-//
-
 int	main(int argc, char**argv)
 {
 	char	*line;
 	int		fd = argc > 1 ? open(argv[1], O_RDONLY) : 0;
 	int		status;
-    
+	
 	printf("fd = %d\n", fd);
 
 	while ((status = get_next_line(fd, &line)) == 1)
