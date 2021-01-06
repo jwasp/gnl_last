e204d9f6e0330eb9cd31174fea1d1e9ac5220417
jwasp <nigushaa@yandex.ru>
2021-01-06 12:24:38 +0300

    delete find_n

--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE (4*1024)
+#  define BUFFER_SIZE 1024
 # endif
 # include <stdlib.h>
 # include <unistd.h>
@@ -22,6 +22,5 @@ size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *src);
 char	*ft_strchr(char *str, int ch);
-int		find_n(char *str);
 int		get_next_line(int fd, char **line);
 #endif
